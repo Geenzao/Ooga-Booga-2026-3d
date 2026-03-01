99f656d1c2f55dab94eb54a83d9e325516f86e0e
Antoine Chabanel <antoinebaptistemozart2@gmail.com>
2026-03-01 18:09:26 +0100

    fix premiere partie

--- Assets/Script/RescaleComputer.cs
+++ Assets/Script/RescaleComputer.cs
@@ -13,7 +13,10 @@ public class RescaleComputer : MonoBehaviour
     public void getBackToRealOriginalSize()
     {
         gameObject.GetComponent<RectTransform>().sizeDelta = realDefaultSize;
+        //gameObject.GetComponent<RectTransform>().anchoredPosition3D = new(gameObject.GetComponent<RectTransform>().position.x, 0.004486f, gameObject.GetComponent<RectTransform>().position.z);
         ModeleEcran.localScale = realDefaultScale;
+        defaultScale = realDefaultScale;
+        defaultSize = realDefaultSize;
     }
 
     public void getBackToOriginalSize()
